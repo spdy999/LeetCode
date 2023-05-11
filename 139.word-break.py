5de626a10c9a67b6742de4a45988c26979025edb
Spdy999 <1a1a1a.262626.0010@gmail.com>
2023-05-11 14:32:16 -0400

    add 139.word-break.py

---
+++ 139.word-break.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=139 lang=python3
+#
+# [139] Word Break
+#
+
+
+# @lc code=start
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[n] = True
+
+        for i in range(n - 1, -1, -1):
+            for w in wordDict:
+                if i + len(w) <= n and s[i : i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+
+                if dp[i]:
+                    break
+
+        return dp[0]
+
+
+# @lc code=end
